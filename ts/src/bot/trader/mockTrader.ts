9842dd128625129a14edd1c7d394cf7f85ecb94e
anmc <lamylanmc@hotmail.com>
2021-06-08 13:43:25 +0800

    fix sell price

--- ts/src/bot/trader/mockTrader.ts
+++ ts/src/bot/trader/mockTrader.ts
@@ -14,19 +14,17 @@ namespace bot { export namespace trader {
             return this.balances
         }
 
-        private async _buy( baseAsset: string, quoteAsset: string, closePrice: number, quantity: number){
+        async buy( baseAsset: string, quoteAsset: string, closePrice: number, quantity: number ){
             this.balances[baseAsset] = (this.balances[baseAsset] || 0)+quantity
             this.balances[quoteAsset] = (this.balances[quoteAsset] || 0)-quantity*closePrice
-        }
 
-
-        async buy( baseAsset: string, quoteAsset: string, closePrice: number, quantity: number ){
-            await this._buy(baseAsset, quoteAsset, closePrice, quantity)
             super.buy(baseAsset,quoteAsset,closePrice,quantity)
         }
 
         async sell( baseAsset: string, quoteAsset: string, closePrice: number, quantity: number ){
-            await this._buy( quoteAsset, baseAsset, quantity/closePrice, quantity/closePrice)
+            this.balances[baseAsset] = (this.balances[baseAsset] || 0)-quantity
+            this.balances[quoteAsset] = (this.balances[quoteAsset] || 0)+quantity*closePrice
+
             super.sell(baseAsset,quoteAsset,closePrice,quantity)
         }
     }
