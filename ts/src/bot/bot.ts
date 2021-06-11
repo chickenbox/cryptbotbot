ce88119a67b8d2c4e96c81c8d0612d55da8f0ced
anmc <lamylanmc@hotmail.com>
2021-06-11 08:20:00 +0800

    local minimum

--- ts/src/bot/bot.ts
+++ ts/src/bot/bot.ts
@@ -234,16 +234,20 @@ namespace bot {
                     const secLastIdx = trendWatcher.data.length-2
                     
                     let valley = false
-                    let localPeak = false
+                    let localMinimum = false
                     let dropping = false
                     let peak = false
 
                     if( trendWatcher.dDataDt[secLastIdx]<0 && trendWatcher.dDataDt[lastIdx]>=0 ){
                         valley = true
                     }
-                    if( trendWatcher.data[secLastIdx].price<trendWatcher.data[lastIdx].price){
-                        localPeak = true
-                    }
+                    const localRegion = this.smoothAmount*2
+                    localMinimum = true
+                    for( let i=1; i<localRegion; i++ ){
+                        if( lastIdx-i>=0 && trendWatcher.data[lastIdx]>trendWatcher.data[lastIdx-i] ){
+                            localMinimum = false
+                        }
+                    }                    
                     if( trendWatcher.dDataDt[secLastIdx]>0 && trendWatcher.dDataDt[lastIdx]<=0 ){
                         peak = true
                     }
@@ -255,9 +259,7 @@ namespace bot {
 
                     let action = "none"
 
-                    if( valley && !localPeak ){
-                        
-
+                    if( valley && localMinimum ){
                         if( this.allow.buy)
                             action = "buy"
                     }else{
