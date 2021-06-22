42389bce22aa36b4208ee1a68a142188932830d4
anmc <lamylanmc@hotmail.com>
2021-06-22 15:29:09 +0800

    remove SUN update smooth amount

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -3,7 +3,7 @@ setTimeout( function(){
         homingAsset: "USDT",
         interval: "30m",
         minHLRation: 1.1,
-        smoothAmount: 10,
+        smoothAmount: 20,
         maxAllocation: 1/10,
         logLength: 10000,
         holdingBalance: 10000,
@@ -11,7 +11,7 @@ setTimeout( function(){
         mockRun: process.argv[4]=="true",
         apiKey: process.argv[2],
         apiSecure: process.argv[3],
-        whiteList:["DOGE","BTC","ETH","BNB","BUSD","USDC","TUSD","PAX","BCH","EOS","XRP","TRX","ETC","LTC","ADA","ATOM","DASH","HBAR","LINK","MATIC","NEO","QTUM","WRX","XMR","1INCH","AAVE","ALGO","ALPHA","AUDIO","AUD","BAL","BAND","BAT","BEAM","BTT","CHZ","COMP","CRV","DOT","ENJ","EUR","FIL","FTM","GBP","GRT","HNT","JST","KNC","MITH","MKR","OMG","ONT","PAXG","RUNE","ZEN","ZEC","YFII","YFI","XLM","WAVES","VET","UNI","TOMO","SXP","SUSHI","SUN","SRM","SOL","SNX","SHIB"]
+        whiteList:["DOGE","BTC","ETH","BNB","BUSD","USDC","TUSD","PAX","BCH","EOS","XRP","TRX","ETC","LTC","ADA","ATOM","DASH","HBAR","LINK","MATIC","NEO","QTUM","WRX","XMR","1INCH","AAVE","ALGO","ALPHA","AUDIO","AUD","BAL","BAND","BAT","BEAM","BTT","CHZ","COMP","CRV","DOT","ENJ","EUR","FIL","FTM","GBP","GRT","HNT","JST","KNC","MITH","MKR","OMG","ONT","PAXG","RUNE","ZEN","ZEC","YFII","YFI","XLM","WAVES","VET","UNI","TOMO","SXP","SUSHI","SRM","SOL","SNX","SHIB"]
     })
     const httpHelper = new bot.helper.HttpHelper(b,3333)
     b.run()
