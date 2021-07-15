d1ce12daf419e85e92a9273ed35e2395cde75323
anmc <lamylanmc@hotmail.com>
2021-07-15 16:10:59 +0800

    enlarge localstorage

--- ts/src/init.ts
+++ ts/src/init.ts
@@ -7,7 +7,7 @@ const LocalStorage = require("node-localstorage").LocalStorage
 const localStorage: {
     getItem( s: string ): string
     setItem( s: string, v: string )
-} = new LocalStorage('./scratch', 50*1024*1024)
+} = new LocalStorage('./scratch', 100*1024*1024)
 
 const fetch = require('node-fetch')
 const CryptoJS = require('crypto-js')
